5d9add562e7c206949ffec099030eec4c432c638
Karl Chvojka <karl.chvojka@gmail.com>
2019-05-07 22:23:17 +0000

    Finished todays work.

--- src/App.jsx
+++ src/App.jsx
@@ -21,25 +21,37 @@ class App extends Component {
         }
       ]
     };
+    this.addMessageItem = this.addMessageItem.bind(this);
   }
-  componentDidMount() {
-  console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);
-}
+  // componentDidMount() {
+  //   console.log("componentDidMount <App />");
+  //   setTimeout(() => {
+  //     console.log("Simulating incoming message");
+  //     // Add a new message to the list of messages in the data store
+  //     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+  //     const messages = this.state.messages.concat(newMessage)
+  //     // Update the state of the app component.
+  //     // Calling setState will trigger a call to render() in App and all child components.
+  //     this.setState({messages: messages})
+  //   }, 3000);
+  // }
+  randomGenId() {
+    let min = 2
+    let max = 40000
+    let random = Math.floor(Math.random() * (max - min)) + min
+    return random
+  };
+  addMessageItem(input) {
+    const newMessage = {id: this.randomGenId(), username: this.state.currentUser.name, content: input}
+    const newDats = this.state.messages.concat(newMessage);
+    this.setState({ messages: newDats});
+  }
   render() {
     return (
       <div>
         <NavBar />
         <MessageList messages={this.state.messages} />
-        <ChatBar currentUser={this.state.currentUser} />
+        <ChatBar currentUser={this.state.currentUser} addMessage={this.addMessageItem} />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 
+
 class ChatBar extends Component {
+  // Get the input field
+  handleKeyPress = (event) => {
+    if(event.keyCode == 13) {
+      this.props.addMessage(event.target.value);
+      event.target.value = '';
+    }
+  }
   render () {
     console.log(this.props)
+
+
     return (
       <footer className="chatbar">
         <input className="chatbar-username" defaultValue={this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" name="messageInput" placeholder="Type a message and hit ENTER" onKeyDown={this.handleKeyPress}/>
       </footer>
     )
   }
