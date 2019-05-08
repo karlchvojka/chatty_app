0d5c10fe301a86ebe8b7487f943375539b1dfdb9
Karl Chvojka <karl.chvojka@gmail.com>
2019-05-08 20:43:37 +0000

    Connection working

--- src/App.jsx
+++ src/App.jsx
@@ -22,19 +22,23 @@ class App extends Component {
       ]
     };
     this.addMessageItem = this.addMessageItem.bind(this);
+    this.socket = new WebSocket('ws://localhost:3001');
   }
-  // componentDidMount() {
-  //   console.log("componentDidMount <App />");
-  //   setTimeout(() => {
-  //     console.log("Simulating incoming message");
-  //     // Add a new message to the list of messages in the data store
-  //     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-  //     const messages = this.state.messages.concat(newMessage)
-  //     // Update the state of the app component.
-  //     // Calling setState will trigger a call to render() in App and all child components.
-  //     this.setState({messages: messages})
-  //   }, 3000);
-  // }
+  componentDidMount() {
+    this.socket.onopen = event => {
+      console.log('Connected to Server.');
+    };
+    // console.log("componentDidMount <App />");
+    // setTimeout(() => {
+    //   console.log("Simulating incoming message");
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    //   const messages = this.state.messages.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({messages: messages})
+    // }, 3000);
+  }
   randomGenId() {
     let min = 2
     let max = 40000
